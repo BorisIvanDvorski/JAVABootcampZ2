6f20fd37773f4b2cff86365a983044d42c14223d
Boris Ivan Dvorski <bdvorski@vub.hr>
2023-03-09 21:14:46 +0100

    Added required functionalities, without file output/input

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner kmInput = new Scanner(System.in);
+        double brojKM=0, conversionFactor=0;
+
+        while(brojKM<=0){
+            System.out.print("Unesite broj kilometara: ");
+            brojKM= kmInput.nextDouble();
+            if(brojKM<=0){
+                System.out.println("Broj kilometara mora biti pozitivan broj.");
+            }
+        }
+
+        while(conversionFactor<=0){
+            System.out.print("Unesite konverzijski faktor: ");
+            conversionFactor= kmInput.nextDouble();
+            if(conversionFactor<=0){
+                System.out.println("Konverzijski faktor mora biti pozitivan broj.");
+            }
+        }
+
+        int brojMi = (int)(brojKM/conversionFactor);
+
+        System.out.println("Uneseno je "+brojKM+" kilometara, što je "+brojMi+" milja.");
+
     }
 }
